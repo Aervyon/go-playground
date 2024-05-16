b26b124c59c9c7b1d189cc6d778edafbdce513cb
Aervyon <aervy@aervyon.com>
2024-05-16 02:12:33 -0500

    fix(sessions): Redo the way sessions are handled

--- http/auth.go
+++ http/auth.go
@@ -3,7 +3,6 @@ package http
 import (
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/Aervyon/go-playground/models"
 	"github.com/Aervyon/go-playground/utils"
@@ -59,49 +58,8 @@ func AuthUser(db *gorm.DB, sessionManager *scs.SessionManager) http.HandlerFunc 
 			render.JSON(w, r, render.M{"code": http.StatusInternalServerError, "message": "access token generation failed"})
 		}
 
-		sessionManager.Put(r.Context(), "auth", token.ID+"."+token.Token)
+		sessionManager.Put(r.Context(), "session", user.ID)
 
 		render.JSON(w, r, render.M{"code": 201, "message": "Created Token", "token": token.Token, "tokenType": "Bearer"})
-	}
-}
-
-func CheckSession(db *gorm.DB, sessionManager *scs.SessionManager) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		auth := sessionManager.GetString(r.Context(), "auth")
-
-		pair := strings.Split(auth, ".")
-		if len(pair) != 2 {
-			w.WriteHeader(http.StatusUnauthorized)
-			render.JSON(w, r, render.M{"code": http.StatusUnauthorized, "message": "Unauthorized"})
-			return
-		}
-
-		// find the session in the database
-		var token models.Token
-		db.Model(&models.Token{}).Find(&token, "ID = ?", pair[0])
-		if token.Hash == "" {
-			w.WriteHeader(http.StatusUnauthorized)
-			render.JSON(w, r, render.M{"code": http.StatusUnauthorized, "message": "Unauthorized"})
-			return
-		}
-
-		isValid, _, err := argon2id.CheckHash(pair[1], token.Hash)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			render.JSON(w, r, render.M{
-				"code":    http.StatusInternalServerError,
-				"message": "Unauthorized",
-				"error":   err.Error(),
-			})
-			return
-		}
-
-		if !isValid {
-			w.WriteHeader(http.StatusUnauthorized)
-			render.JSON(w, r, render.M{"code": http.StatusUnauthorized, "message": "Unauthorized"})
-			return
-		}
-
-		render.JSON(w, r, render.M{"code": http.StatusOK, "message": "Hi user with ID" + token.UID})
 	}
 }
--- http/user.go
+++ http/user.go
@@ -1,11 +1,13 @@
 package http
 
 import (
+	"errors"
 	"net/http"
 
 	"log"
 
 	"github.com/Aervyon/go-playground/utils"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/render"
 	"gorm.io/gorm"
 )
@@ -45,6 +47,44 @@ func CreateUser(db *gorm.DB) http.HandlerFunc {
 		}
 
 		render.JSON(w, r, render.M{"code": http.StatusOK, "message": "Saved your info. Welcome " + user.Username})
+	}
+}
+
+func GetSelfAccount(db *gorm.DB, session *scs.SessionManager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		uid := session.GetString(r.Context(), "session")
+		if uid == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			render.JSON(w, r, render.M{"code": http.StatusUnauthorized, "message": "unauthorized"})
+			return
+		}
+
+		var user utils.UserModel
+		transaction := db.Model(&utils.UserModel{}).Limit(1).Find(&user, "ID = ?", uid)
+		if transaction.Error != nil && errors.Is(transaction.Error, gorm.ErrRecordNotFound) {
+			log.Println("No record found for authentication user", uid)
+			w.WriteHeader(http.StatusNotFound)
+			render.JSON(w, r, render.M{
+				"code":    http.StatusNotFound,
+				"message": "Account " + uid + " not found",
+			})
+		}
+		if transaction.Error != nil {
+			log.Println(transaction.Error.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			render.JSON(w, r, render.M{
+				"code":    http.StatusInternalServerError,
+				"message": "Error Occurred getting your account",
+				"error":   transaction.Error.Error(),
+			})
+			return
+		}
+
+		render.JSON(w, r, render.M{
+			"code":    http.StatusOK,
+			"message": "OK",
+			"account": user,
+		})
 	}
 }
 

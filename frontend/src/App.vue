b82d5f93d5425dec2ad07e6082bd0b69c564ae59
Aervyon <aervy@aervyon.com>
2024-05-25 20:46:10 -0500

    feat: Add generic navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import Navbar from "./components/Navbar.vue"
 </script>
 
 <template>
-  <RouterView></RouterView>
+  <Navbar></Navbar>
+  <RouterView class="min-h-lvh"></RouterView>
 </template>
